24ee6a27edd78ac41662518244f97142c4f5c45f
RafaelJosePortoSilva <rafaelporto.dev@gmail.com>
2023-10-13 11:35:46 -0300

    Adicionado o panel  ao codigo

--- src/Index.java
+++ src/Index.java
@@ -2,7 +2,27 @@ import javax.swing.*;
 
 public class Index extends JFrame {
 
+    private JLabel lbl_Title;
+    private JPanel Index_Panel;
 
+    public Index(){
+
+        setContentPane(Index_Panel);
+
+        setTitle("Biblioteca");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1080,720);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+
+        new Index();
+
+
+    }
 
 
 }
