0e6e08a4000c904c1a90bb77c610203989cec6ad
RafaelJosePortoSilva <rafaelporto.dev@gmail.com>
2023-10-13 14:24:10 -0300

    integrando csv

---
+++ src/Obj/Livro.java
@@ -0,0 +1,14 @@
+package Obj;
+
+public class Livro {
+
+    private codigo;
+    private titulo;
+    private autor;
+    private ano_publicacao;
+
+    /*
+    ISBN,Book-Title,Book-Author,Year-Of-Publication,Publisher,Image-URL-S,Image-URL-M,Image-URL-L
+    */
+
+}
