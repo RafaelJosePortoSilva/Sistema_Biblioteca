a023672a4d570dd57337f7d73be6df75fbd1c111
RafaelJosePortoSilva <rafaelporto.dev@gmail.com>
2023-10-15 02:11:39 -0300

    condicoes de funcionamento em pesquisar_livros

--- src/Index.java
+++ src/Index.java
@@ -2,6 +2,8 @@ import Obj.Database;
 import Obj.Livro;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Index extends JFrame {
 
@@ -9,7 +11,7 @@ public class Index extends JFrame {
     private JPanel Index_Panel;
     private JTextField txt_pesquisar;
     private JButton btn_pesquisar;
-    private JList list1;
+    private JList list_pesquisar;
     private JLabel lbl_livrosCarregados;
 
     public Index(){
@@ -21,6 +23,7 @@ public class Index extends JFrame {
         setSize(1080,720);
         setLocationRelativeTo(null);
         setVisible(true);
+
     }
 
 
@@ -35,8 +38,21 @@ public class Index extends JFrame {
         index.lbl_livrosCarregados.setText("Livros carregados: \n" + df.length);
 
 
-
+        index.btn_pesquisar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                pesquisar_livros(index.txt_pesquisar,index.list_pesquisar);
+            } // end actionPerformed
+        }); // end addActionListener
     }
 
+    public static void pesquisar_livros(JTextField txt, JList list){
+
+        String txtPesquisa;
+
+        txtPesquisa = txt.getText();
+        if (txtPesquisa.isEmpty()){return;}
+
+    }
 
 }
